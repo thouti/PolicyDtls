408ab20056792979ebe312ae11682f5bd513a20d
srinivas <srinivas.thouti@gmail.com>
2024-01-27 23:05:00 +0530

    ttttt

--- src/main/java/com/licagent/LicAgentInfoApplication.java
+++ src/main/java/com/licagent/LicAgentInfoApplication.java
@@ -2,7 +2,7 @@ package com.licagent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-// this is main class for Spring boot create 123
+// this is main class for Spring boot create
 @SpringBootApplication
 public class LicAgentInfoApplication {
 
